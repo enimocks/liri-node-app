eb40d11ad3bef8ec3eaaeaf94393f21449a67ec5
Edward Nimocks <enimocks@gmail.com>
2018-04-25 14:59:06 -0400

    add dependencies, node modules, and variable init

--- liri.js
+++ liri.js
@@ -1,2 +1,13 @@
-var dotEnv = require('dotenv').config();
+
+var dotenv = require('dotenv').config();
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
+
+var keys = require('./keys.js');
+
+var command = process.argv[2];
+var input = process.argv[3];
 
