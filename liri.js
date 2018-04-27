d728345c916bf9ef0cc49d5773d025229c63d323
Edward Nimocks <enimocks@gmail.com>
2018-04-26 22:40:56 -0400

    templating tweaks

--- liri.js
+++ liri.js
@@ -5,7 +5,6 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require('fs');
-var omdb = require('omdb');
 
 // get twitter and spotify API key info
 var keys = require('./keys.js');
@@ -72,16 +71,24 @@ function spotify(input) {
 
     var songInfo = data.tracks.items[0];
 
-    console.log( `Song Name: ${songInfo.name}` );
-    console.log( `Artist(s): ${songInfo.album.artists[0].name}` );
-    console.log( `Album: ${songInfo.album.name}` );
-    console.log( `Song Preview: ${songInfo.preview_url}` );
+    console.log(
+`
+=================================================
+Song Name:     ${songInfo.name}
+Artist(s):     ${songInfo.album.artists[0].name}
+Album:         ${songInfo.album.name}
+Song Preview:  ${songInfo.preview_url}
+=================================================
+`
+    );
   });
 }
 
-// OBDB FUNCTIONt
+// OMDB FUNCTION
 // ===============================================================================
-  var nodeArgs = process.argv;
+// ********************************************************************************
+// ********************************************************************************
+  var nodeArgs = process.argv; // questions about movies with spaces start here
 
   // Create an empty variable for holding the movie name
   var input = "";
@@ -91,67 +98,47 @@ function spotify(input) {
   for (var i = 3; i < nodeArgs.length; i++) {
 
     if (i > 3 && i < nodeArgs.length) {
-
       input = input + "+" + nodeArgs[i];
-
     }
-
     else {
-
       input += nodeArgs[i];
-
     }
   }
 
-function movie(input) { 
-
-    var nodeArgs = process.argv;
-
-    // Create an empty variable for holding the movie name
-    var input = "";
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "+"s
-    for (var i = 3; i < nodeArgs.length; i++) {
-
-      if (i > 3 && i < nodeArgs.length) {
-
-        input = input + "+" + nodeArgs[i];
-
-      }
-
-      else {
-
-        input += nodeArgs[i];
-
-      }
-    }
+function movie(input) {
   
   var queryURL = `http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`
 
   request(queryURL, function (error, response, body) {
-console.log(queryURL);
+    console.log(queryURL)
 
     if (!input) {
-      input = 'Mr Nobody';
+      input = 'Mr. Nobody';
     }
     
     // If the request is successful
     if (!error && response.statusCode === 200) {
 
+      var json = JSON.parse(body);
+
       console.log(
-        `
-        =================================================================================
-        Title:                         ${JSON.parse(body).Title}
-        Release Year:                  ${JSON.parse(body).Year}
-        IMDB Rating:                   ${JSON.parse(body).imdbRating}
-        Rotten Tomatoes Rating:        ${JSON.parse(body).Ratings[1].Value}
-        Production Country(s):         ${JSON.parse(body).Country}
-        Language:                      ${JSON.parse(body).Language}
-        Plot:                          ${JSON.parse(body).Plot}
-        Actors:                        ${JSON.parse(body).Actors}
-        =================================================================================
-        `
+`
+============================================================================
+Release Year:          ${json.Year}
+Title:                 ${json.Title}
+IMDB Rating:           ${json.imdbRating}
+RT Rating:             ${json.Ratings[1].Value}
+Production Country:    ${json.Country}
+Language:              ${json.Language}
+Plot:                  ${json.Plot}
+Actors:                ${json.Actors}
+============================================================================
+`
       );
     }
   });
+}
+
+function doIt() {
+  
 }
